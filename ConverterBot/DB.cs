412b5af9b9d06b3dd986b0d840c3be7b41d652e4
Филатов Антон Владимирович <anton.filatov@orspo>
2020-10-30 18:37:50 +0300

    Beginnings of DB

---
+++ ConverterBot/DB.cs
@@ -0,0 +1,27 @@
+﻿using ConverterBot.Models;
+using LiteDB;
+
+namespace ConverterBot
+{
+    public static class DB
+    {
+        private static LiteDatabase Database;
+
+        static DB( )
+        {
+            Database = new LiteDatabase( @"mscb.db" );
+        }
+
+        public static string[] GetServicesForChat( long chatId )
+        {
+            var collection = Database.GetCollection<ServicesInChat>( "ServicesInChats" );
+            return collection.FindOne( _ => _.ChatId == chatId ).Services;
+        }
+
+        public static void SetServicesForChat( long chatId, string[] services )
+        {
+            var collection = Database.GetCollection<ServicesInChat>( "ServicesInChats" );
+            collection.Insert( new ServicesInChat( chatId, services ) );
+        }
+    }
+}
---
+++ ConverterBot/Models/ServicesInChat.cs
@@ -0,0 +1,14 @@
+﻿namespace ConverterBot.Models
+{
+    public class ServicesInChat
+    {
+        public long ChatId { get; }
+        public string[] Services { get; }
+
+        public ServicesInChat( long chatId, string[] services )
+        {
+            ChatId = chatId;
+            Services = services;
+        }
+    }
+}
