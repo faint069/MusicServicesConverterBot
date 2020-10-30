a4eeb4f8547a3257a69bb6715cac48b0046e1e4f
Филатов Антон Владимирович <anton.filatov@orspo>
2020-10-30 15:04:19 +0300

    Youtube music builder(just search string)

---
+++ ConverterBot/Builders/YoutubeMusicBuilder.cs
@@ -0,0 +1,16 @@
+﻿using ConverterBot.Models;
+
+namespace ConverterBot.Builders
+{ 
+ /// <summary>
+ /// While Youtube Music doesn't have API this builder just returns URI to Youtube Music search page 
+ /// </summary>
+    public class YoutubeMusicBuilder : IBuilder
+    {
+        public string SearchMusic( IMusic musicToSearch )
+        {
+            return "https://music.youtube.com/search?q=" + 
+                   musicToSearch.QueryString(  ).Replace( ' ', '+' );
+        }
+    }
+}
