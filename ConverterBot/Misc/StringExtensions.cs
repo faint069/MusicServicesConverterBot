7e30f5a954a64c87329400feb66f5f93ee7ad136
Филатов Антон Владимирович <anton.filatov@orspo>
2020-11-04 13:39:37 +0300

    Added method to remove brackets in strings

---
+++ ConverterBot/Misc/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace ConverterBot.Misc
+{
+  public static class StringExtensions
+  {
+    private static string RemoveBrackets( string input )
+    {
+      if ( !( input.Contains( '(' ) && input.Contains( ')' ) ) )
+      {
+        return input;
+      }
+
+      string result = input;
+      if ( input.IndexOf( '(' ) > input.IndexOf( ')' ) )
+      {
+        result = input.Remove( input.IndexOf( ')' ),
+        input.IndexOf( '(' ) - input.IndexOf( ')' ) + 1 );
+      }
+      
+      if ( input.IndexOf( '(' ) < input.IndexOf( ')' ) )
+      {
+        result = input.Remove( input.IndexOf( '(' ),
+        input.IndexOf( ')' ) - input.IndexOf( '(' ) + 1 );
+      }
+      
+      return result.Trim( );
+    }
+
+    /// <summary>
+    /// Удалаяет из строки круглые скобки и все что находится между ними
+    /// </summary>
+    /// <param name="input">Входная строка</param>
+    /// <returns></returns>
+    public static string RemoveAllBrackets( string input )
+    {
+      while ( input != RemoveBrackets( input ) )
+      {
+        input = RemoveBrackets( input );
+      }
+
+      return input;
+    }
+  }
+}
