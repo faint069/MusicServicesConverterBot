d1ba0b94a7cc0927702195d9d0c95fb77716c85c
Филатов Антон Владимирович <anton.filatov@orspo>
2020-11-05 21:19:07 +0300

    loosed messages class

---
+++ ConverterBot/Localization/Messages.cs
@@ -0,0 +1,8 @@
+﻿namespace ConverterBot.Localization
+{
+  public static class Messages
+  {
+    public static LocalizedString GreetingMessage => new LocalizedString( "HelloMessage" );
+    
+  }
+}
